014fd7dc8601a1eeeeead082243eb72a6256d2ab
zapashniy_aleksandr <aleksandr.zapashniy@gorparkovki.ru>
2024-12-24 12:10:41 +0300

    preparation for api 34

--- library-gms-geofence/build.gradle.kts
+++ library-gms-geofence/build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 34
+    namespace = "ru.fabit.gmsgeofence"
 
     defaultConfig {
         minSdk = 24
@@ -30,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     buildFeatures {
